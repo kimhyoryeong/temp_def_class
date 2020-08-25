56fc3e9e29f6d6d17923a3f30d1038f8a430a788
kimhyoryeong <llx@nate.com>
2020-08-25 11:15:43 +0900

    commit-m ,

---
+++ arithmetic/plus.py
@@ -0,0 +1,6 @@
+# -*- coding; utf-8 -*-
+# import calculation
+
+def add(a,b):
+    c = a + b
+    return c
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 # -*- coding; utf-8 -*-
 
 import calculation as cal
-
+from arithmetic import plus
+from arithmetic import subtract
 a = 3
 b = 4
 
 def main():
     print("안녕하세요 ,main 입니다.")
-    print("a + b = ", cal.subtract(a,b))
-    print("a - b =", cal.plus(a, b))
-    print("a * b =", cal.multiple(a, b))
+    print("a + b = ", plus.add(a,b))
+    print("a - b =", subtract.minus(a, b))
 
 if __name__ == "__main__":
-    main()
+    main();
 
+# -*- coding: utf-8 -*-
+from arithmetic import plus as pl
+from arithmetic import subtract as sub
+from datapreprocessing import processing
+from datapreprocessing import importData
+
+a = 3
+b = 4
+
+def main():
+  print("~~ 사칙 연산을 시작합니다 ~~ ")
+  print("a + b =", sub.minus(a, b))
+  print("a - b =", pl.add(a, b))
+  print("~~ 사칙 연산을 종료합니다 ~~ ")
+
+  ## 데이터 전처리 시작
+  data = importData.readData()
+  processing.process_data(data)
+
+if __name__ == "__main__":
+  main()
